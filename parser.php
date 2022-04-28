18c609240113cc646072b0f19e1e24c246ff4a6b
Alexander Mironov <alexan9610@gmail.com>
2022-04-28 04:35:15 +0300

    main work file

--- parser.php
+++ parser.php
@@ -1,3 +1,16 @@
 <?php
 
 // php parser.php ./acess_log
+require_once __DIR__ . '/src/vendor/autoload.php';
+
+use App\Roistat;
+
+try {
+    $parser = new Roistat\Parser('access_log');
+
+    $parser->loadFrom('./test-sample')->fetchParseStatictics()->saveTo('JSON', 'sample.json');
+
+    $parser->close();
+} catch (\Exception $error) {
+    echo $error->getMessage();
+}
